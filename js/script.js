a7d5cd87ece796049a7b3c889d0f24dce0dde36e
Rizvanov Linaz <rizvan.linaz@gmail.com>
2020-04-22 21:03:03 +0300

    mobile menu

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ $( () => {
 		var windowTop = $(window).scrollTop();
         windowTop > 250 ? $('.header').addClass('header-shadow') : $('.header').removeClass('header-shadow');
 		windowTop > 250 ? $('.logo').addClass('logo-scroll') : $('.logo').removeClass('logo-scroll');
+		windowTop > 250 ? $('.menu-btn').addClass('menu-btn-scroll') : $('.menu-btn').removeClass('menu-btn-scroll');
+		windowTop > 250 ? $('.mobile-menu').addClass('mobile-menu-scroll') : $('.mobile-menu').removeClass('mobile-menu-scroll');
+
+		
+
         
   
     // windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
@@ -27,34 +32,56 @@ $( () => {
 
 });
 
-const authButton = document.querySelector('#auth');
-const mailButton = document.querySelector('#mail');
+// const authButton = document.querySelector('#auth');
+// const mailButton = document.querySelector('#mail');
 
-const modal = document.querySelector('#modalAuth');
-const modalMail = document.querySelector('#modalMail');
+// const modal = document.querySelector('#modalAuth');
+// const modalMail = document.querySelector('#modalMail');
 
-const closeAuth = document.querySelector('#closeModalAuth');
-const closeMail = document.querySelector('#closeModalMail');
+// const closeAuth = document.querySelector('#closeModalAuth');
+// const closeMail = document.querySelector('#closeModalMail');
 
 
-authButton.addEventListener('click', function (event) {
-    modal.classList.add("modal-open");
+// authButton.addEventListener('click', function (event) {
+//     modal.classList.add("modal-open");
+// });
+// closeAuth.addEventListener('click', function (event) {
+//     modal.classList.remove("modal-open");
+// });
+
+// mailButton.addEventListener('click', function (event) {
+//     modalMail.classList.add("modal-open");
+// });
+// closeMail.addEventListener('click', function (event) {
+//     modalMail.classList.remove("modal-open");
+// });
+
+// mail modal
+$('#mail').click(function(e){
+	e.preventDefault();
+	$('#modalMail').addClass('modal-open');
 });
-closeAuth.addEventListener('click', function (event) {
-    modal.classList.remove("modal-open");
+$('#closeModalMail').click(function(e){
+	e.preventDefault();
+	$('#modalMail').removeClass('modal-open');
 });
 
-mailButton.addEventListener('click', function (event) {
-    modalMail.classList.add("modal-open");
+// auth modal
+$('#auth').click(function(e){
+	e.preventDefault();
+	$('#modalAuth').addClass('modal-open');
 });
-closeMail.addEventListener('click', function (event) {
-    modalMail.classList.remove("modal-open");
+$('#closeModalAuth').click(function(e){
+	e.preventDefault();
+	$('#modalAuth').removeClass('modal-open');
 });
 
-
-
-
-
+// open mobile menu
+$('.menu-btn').click(function(e){
+	e.preventDefault();
+	$('.mobile-menu').slideToggle();
+	$(this).toggleClass('mobile-menu-open');
+});
 
 
 
