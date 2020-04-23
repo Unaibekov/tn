40e30368b0da6148a0202e25427a441c65a46bbe
Rizvanov Linaz <rizvan.linaz@gmail.com>
2020-04-23 16:19:36 +0300

    favicon

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $( () => {
 	//Smooth Scrolling Using Navigation Menu
 	$('a[href*="#"]').on('click', function(e){
 		$('html,body').animate({
-			scrollTop: $($(this).attr('href')).offset().top - 60
+			scrollTop: $($(this).attr('href')).offset().top - 50
 		},100);
 		e.preventDefault();
 	});	
